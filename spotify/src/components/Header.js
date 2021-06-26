f88780217269a25f0e63a0661589b6cfd5fd7109
Elijah Littell <elittell214@gmail.com>
2021-06-26 01:16:33 -0400

    Added things for the header

--- spotify/src/components/Header.js
+++ spotify/src/components/Header.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import "./../css/Header.css";
 
 function Header() {
     return (
         <div className="header">
-            <h3>this is theee Header.jsss </h3>
+            <div className="back-forward">
+                <h3>back</h3>
+                <h3>forward</h3>
+            </div>
+            <div className="button-signin">
+                <button className="sign-up">Sign Up</button>
+                <button className="log-in">Log In</button>
+            </div>
         </div>
     )
 }
