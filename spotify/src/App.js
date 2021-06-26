8ae14212d817f9670913d3ee01e3370f7faf82d5
Elijah Littell <elittell214@gmail.com>
2021-06-26 00:39:28 -0400

    Made a app left div and app right div

--- spotify/src/App.js
+++ spotify/src/App.js
@@ -1,9 +1,18 @@
 import './css/App.css';
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+import BrowseMusic from "./components/BrowseMusic";
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello World</h1>
+      <div className="app-left">
+        <Sidebar />
+      </div>
+      <div className="app-right">
+        <Header />
+        <BrowseMusic />
+      </div>
     </div>
   );
 }
