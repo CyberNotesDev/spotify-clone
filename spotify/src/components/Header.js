7774355166258a3a96c23f7cc923fe0293eccade
Elijah Littell <elittell214@gmail.com>
2021-06-26 00:49:47 -0400

    Added classname

--- spotify/src/components/Header.js
+++ spotify/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header() {
     return (
-        <div>
+        <div className="header">
             <h3>this is theee Header.jsss </h3>
         </div>
     )
