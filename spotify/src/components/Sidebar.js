aa963c0e805713829a01946458815b79abde3578
Elijah Littell <elittell214@gmail.com>
2021-06-26 01:16:55 -0400

    Added classname

--- spotify/src/components/Sidebar.js
+++ spotify/src/components/Sidebar.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import "./../css/Sidebar.css";
 
 function Sidebar() {
     return (
-        <div>
+        <div className="sidebar">
             <h1>this is the sidebar</h1>
         </div>
     )
