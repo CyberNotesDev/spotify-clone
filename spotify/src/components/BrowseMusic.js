265072934874220d3affdfbf674ec5b3ba0aea0e
Elijah Littell <elittell214@gmail.com>
2021-06-26 16:06:44 -0400

    Added more browsemusicrows and modified them

--- spotify/src/components/BrowseMusic.js
+++ spotify/src/components/BrowseMusic.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import "./../css/Browsemusic.css";
+import BrowseMusicRow from './BrowseMusicRow';
 
 function BrowseMusic() {
     return (
         <div className="browse-music">
-            <h1>Hi</h1>
+            <BrowseMusicRow title="The state of music today" description={null} seeAll />
+            <BrowseMusicRow title="Focus" description="Music to help you concentrate." seeAll />
+            <BrowseMusicRow title="Mood" description="Playlists to match your mood." seeAll />
+            <BrowseMusicRow title="Popular new releases" description={null} seeAll />
+            <BrowseMusicRow title="Saturday fun" description={null} seeAll />
+            <BrowseMusicRow title="Soundtrack your home" description={null} seeAll={false} />
         </div>
     )
 }
