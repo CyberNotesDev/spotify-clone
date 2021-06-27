0b1a5f40a151f4e4a069ebd0a1b7016883ed14dc
Elijah Littell <elittell214@gmail.com>
2021-06-27 14:32:10 -0400

    changed the html layout

--- spotify/src/components/BrowseMusicRow.js
+++ spotify/src/components/BrowseMusicRow.js
@@ -3,17 +3,15 @@ import "./../css/Browsemusicrow.css";
 
 function BrowseMusicRow({ title, description, seeAll}) {
     return (
-        <div className="browse-music-row">
-            <div className="browse-music-row-top">
-                <div className="browse-music-title">
-                    <h1>{title}</h1>
-                    <p>{description}</p>
-                </div>
-                <div className="browse-music-see-all">
-                    {
-                        seeAll && <h3>See All</h3>
-                    }
-                </div>
+        <div className="browse-music-row-top">
+            <div className="browse-music-title">
+                <h1>{title}</h1>
+                <p>{description}</p>
+            </div>
+            <div className="browse-music-see-all">
+                {
+                    seeAll && <h3>See All</h3>
+                }
             </div>
         </div>
     )
